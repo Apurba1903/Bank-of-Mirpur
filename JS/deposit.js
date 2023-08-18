2c0c5eb969840b0a590fdf7006ee9285c682a070
Apurba1903 <apurba1903@gmail.com>
2023-08-19 04:31:17 +0600

    withdraw

--- JS/deposit.js
+++ JS/deposit.js
@@ -1,14 +1,34 @@
 // Step 1: Add event listener to the deposit button
 document.getElementById('btn-deposit').addEventListener('click', function () {
+
     // Step 2: Get the deposit amount from the deposit input field
     const depositField = document.getElementById('deposit-field');
-    const depositAmount = depositField.value;
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+
     // Step 3: Get the current deposit total
     // For non-input (elements other than input, textarea) use innerText to get the Text
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmount;
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
 
+    // Step 4: Add numbers to set the total deposit
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+    // Set the deposit total
+    depositTotalElement.innerText = currentDepositTotal;
+
+    // Step 5: Get balance current total
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    // Step 6: Calculate current total balance
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    // Set the balance total
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+
+    
 
 
 
